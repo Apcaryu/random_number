33a07ebcd0790e73a9ffdc67ba652620dfd6bc36
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-13 16:49:46 +0100

    add gen_numbers function

--- main.c
+++ main.c
@@ -8,16 +8,37 @@ int	*init_tab(int n_gen)
 	return (tab);
 }
 
+void	gen_numbers(int *tab, int n_gen)
+{
+	int	tmp;
+	int idx;
+
+	idx = 0;
+	while (idx < n_gen)
+	{
+		tmp = (rand() % INT_MAX - 1) / 1000;
+		tab[idx] = tmp;
+		idx++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	int	*tab_numbers;
 	int n_gen;
+	int idx;
 
 	if (argc == 2)
 	{
 		n_gen = atoi(argv[1]);
 		tab_numbers = init_tab(n_gen);
+		gen_numbers(tab_numbers, n_gen);
 	}
-	(void)tab_numbers;
+	idx = 0;
+	while (idx < n_gen)
+	{
+		printf("%d ", tab_numbers[idx]);
+		idx++;
+	}
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 # define MAIN_H
 # include "stdlib.h"
+# include "stdio.h"
+# include "time.h"
+# include "limits.h"
 
 #endif
