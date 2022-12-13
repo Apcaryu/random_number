97b0b72cb1658ad1838b33c60f07cdf570c67b88
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-12-13 16:22:35 +0100

    add init_tab function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
+int	*init_tab(char *n_gen)
+{
+	int	*tab;
+	int	int_n_gen;
+
+	int_n_gen = atoi(n_gen);
+	tab = malloc(sizeof(int) * int_n_gen);
+	return (tab);
+}
+
 int	main(int argc, char *argv[])
 {
-    return (0);
+	int	*tab_numbers;
+	if (argc == 2)
+		tab_numbers = init_tab(argv[1]);
+	(void)tab_numbers;
+	return (0);
 }
